66c54f58306af0475ad41157f111b4055855167b
ryanj359 <124106727+ryanj359@users.noreply.github.com>
2025-04-06 19:11:42 -0400

    Add files via upload

---
+++ plainMap.js
@@ -0,0 +1,6 @@
+let map = L.map('map').setView([37.8, -96], 4);
+
+var tiles = L.tileLayer('https://tile.openstreetmap.org/{z}/{x}/{y}.png', {
+    maxZoom: 19,
+    attribution: '&copy; <a href="http://www.openstreetmap.org/copyright">OpenStreetMap</a>'
+}).addTo(map);
